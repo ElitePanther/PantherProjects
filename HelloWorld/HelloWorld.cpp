85951997fca2b49ac2e22d89c604d9f371bdcb6d
Claudia Luque <clau.spring@gmail.com>
2015-05-02 21:49:18 +0100

    EP-HelloWorld Code Cleanup

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,7 +4,6 @@
 *  main function is the only special function which doesnt has to return
 * something even though it has a non-void return type
 */
-int main()
-{
+int main() {
     std::cout << "Hello World!" << std::endl;
 }

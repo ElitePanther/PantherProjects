8462c9588ac0e9f1f58cddbeb8a1d37851be68ee
Claudia Luque <clau.spring@gmail.com>
2015-05-02 21:42:59 +0100

    EP-HelloWorld Creating Hello World first project

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/**
+*  main function is the only special function which doesnt has to return
+* something even though it has a non-void return type
+*/
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
